3d7ba73b3ff6b511e0387f58f67ee5e82565a872
su-shifang <shifang.su@mloptic.co>
2025-10-30 18:21:52 +0800

    add:mono calibration for colorcamera ui and scripts

---
+++ scripts/monocalibration_colorcamera.py
@@ -0,0 +1,214 @@
+import mlcolorimeter as mlcm
+import cv2
+import os
+import json
+import logging
+import numpy as np
+import tkinter as tk
+from tkinter import messagebox
+import pandas as pd
+from datetime import datetime
+from typing import List,Dict
+
+# Function to show a message box with the light source information
+def show_message(light_source):
+    # Create a hidden root window
+    root = tk.Tk()
+    root.withdraw()  # Hide the root window
+    # Show the message box
+    messagebox.showinfo("提示", "请切换光源到: " + light_source)
+    root.destroy()
+
+# 配置日志
+def setup_logging(log_dir):
+    os.makedirs(log_dir, exist_ok=True)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(os.path.join(log_dir, "serial.log")),
+            logging.StreamHandler(),
+        ],
+    )
+
+# 创建文件夹
+def create_directory(path):
+    os.makedirs(path, exist_ok=True)
+
+# 保存json数据到文件
+def save_json(data, file_path):
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+def process_image(img,img_x=6688,img_y=4764,roi_width=250,roi_height=250):
+    roi_center_x= int(roi_width /2 )
+    roi_center_y= int(roi_height /2 )
+    roi_list = []
+    for x, y in [(img_x, img_y)]:
+        y1,y2 = int(y -roi_center_y), int(y +roi_center_y)
+        x1, x2 = int(x -roi_center_x), int(x + roi_center_x)
+        roi = img[y1: y2, x1: x2]
+        grays = cv2.mean(roi)[0]
+        roi_list.append(grays)
+    return np.mean(roi_list)
+
+def save_results_to_excel(results,out_path):
+    results_df = pd.DataFrame(results)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_excel_path="monocalibration_results" + "_" + timestamp + ".xlsx"
+    os.makedirs(out_path, exist_ok=True)
+    result_file = os.path.join(out_path, output_excel_path)
+    
+    results_df.to_excel(result_file, index=False)
+    logging.info(f"Calibration results saved to {result_file}")
+
+
+def mono_calibration(
+        colorimeter:mlcm.ML_Colorimeter,
+        nd_list:List[mlcm.MLFilterEnum],
+        gray_range:List[float],
+        apturate:str,
+        light_source:str,
+        luminance:float,
+        radiance:float,
+        eye1_path:str,
+        out_path:str,
+        image_point:List[int],
+        roi_size:List[int],
+        binn_selector:mlcm.BinningSelector= mlcm.BinningSelector.Logic,
+        binn_mode:mlcm.BinningMode = mlcm.BinningMode.AVERAGE,
+        binn:mlcm.Binning=mlcm.Binning.ONE_BY_ONE,
+):
+    if len(image_point) >=2:
+        image_x=int(image_point[0])
+        image_y=int(image_point[1])
+    if len(roi_size) >=2:
+        roi_width=int(roi_size[0])
+        roi_height=int(roi_size[1])
+    module_id = 1
+    mono = colorimeter.ml_bino_manage.ml_get_module_by_id(module_id)
+    # set light source
+    mono.ml_set_light_source(light_source)
+    
+    ret = mono.ml_set_binning_selector(binn_selector)
+    if not ret.success:
+        raise RuntimeError("ml_set_binning_selector error")
+
+    # Set binning mode for camera.
+    ret = mono.ml_set_binning_mode(binn_mode)
+    if not ret.success:
+        raise RuntimeError("ml_set_binning_mode error")
+    
+    ret = mono.ml_set_binning(binn)
+    if not ret.success:
+        raise RuntimeError("ml_set_binning error")
+    
+    pixel_format = mlcm.MLPixelFormat.MLBayerRG12
+    ret = mono.ml_set_pixel_format(pixel_format)
+    if not ret.success:
+        raise RuntimeError("ml_set_pixel_format error")
+
+    exposure = mlcm.pyExposureSetting(
+            exposure_mode=mlcm.ExposureMode.Auto, exposure_time=100
+        )
+    results = []
+    for nd in nd_list:
+        # switch nd filter
+        nd_enum = mlcm.MLFilterEnum(int(nd)) # et MLFilterEnum.ND0
+        mono.ml_move_nd_syn(nd_enum)
+        for gray in gray_range:
+            aeparams = mlcm.pyAEParams(dynamic_range=gray,target_max=gray + 0.05,target_min=gray - 0.05, max_time=19000, rate=1000000)
+            ret = mono.ml_update_AE_params(aeparams)
+            mono.ml_set_exposure(exposure=exposure)
+            mono.ml_capture_image_syn()
+            get_image = mono.ml_get_image()
+            # 三个单通道图像
+            X, Y, Z = cv2.split(get_image)
+            exposure_time = mono.ml_get_exposure_time()
+            average_gray_X=process_image(X,image_x,image_y,roi_width,roi_height)
+            process_channel(
+                exposure_time=exposure_time,
+                average_gray=average_gray_X,
+                luminance=luminance,
+                radiance=radiance,
+                gray=gray,
+                nd_enum=nd_enum,
+                results=results,
+                channel_name="X"
+            )
+            
+            average_gray_Y=process_image(Y,image_x,image_y,roi_width,roi_height)
+            process_channel(
+                exposure_time=exposure_time,
+                average_gray=average_gray_Y,
+                luminance=luminance,
+                radiance=radiance,
+                gray=gray,
+                nd_enum=nd_enum,
+                results=results,
+                channel_name="Y"
+            )
+
+            average_gray_Z=process_image(Z,image_x,image_y,roi_width,roi_height)
+            process_channel(
+                exposure_time=exposure_time,
+                average_gray=average_gray_Z,
+                luminance=luminance,
+                radiance=radiance,
+                gray=gray,
+                nd_enum=nd_enum,
+                results=results,
+                channel_name="Z"
+            )
+    # 将results中灰度值在80%的luminance_k和radiance_k写入配置文件，b为0
+    for item in results:
+        if item["Gray Range"] == "80.0%":
+            # 文件夹命名格式为Aperture_NDFilter_LightSource
+            nd_filter = item["NDFilter"]
+            xyz_filter = item["Channel"]
+            file_name="" + apturate + "_" + nd_filter + "_" + xyz_filter + "_" + light_source
+            luminance_k = item["K(L)"]
+            radiance_k = item["K(R)"]
+            luminance_config_path = os.path.join(eye1_path, "Luminance")
+            create_directory(luminance_config_path)
+            radiance_config_path = os.path.join(eye1_path, "Radiance")
+            create_directory(radiance_config_path)
+
+            luminance_file_path = os.path.join(luminance_config_path, file_name)
+            os.makedirs(luminance_file_path, exist_ok=True)
+            # 完整的文件路径
+            luminance_json_file_path= os.path.join(luminance_file_path, "Luminance.json")
+            # 创建要写入的字典
+            data={
+                "Luminance": [[float(luminance_k),0]]
+            }
+            save_json(data, luminance_json_file_path)
+
+            radiance_file_path = os.path.join(radiance_config_path, file_name)
+            os.makedirs(radiance_file_path, exist_ok=True)
+            # 完整的文件路径
+            radiance_json_file_path= os.path.join(radiance_file_path, "Radiance.json")
+            # 创建要写入的字典
+            data1={
+                "Radiance": [[float(radiance_k),0]]
+            }
+            save_json(data1, radiance_json_file_path)
+    
+    save_results_to_excel(results,out_path)
+
+def process_channel(exposure_time,average_gray,luminance,radiance,gray,nd_enum,results,channel_name):
+    gray_ET= average_gray / exposure_time if exposure_time > 0 else 0
+    luminance_k= luminance / gray_ET if gray_ET > 0 else 0
+    radiance_k = radiance / gray_ET if gray_ET > 0 else 0
+    results.append({
+            "Gray Range": f"{gray * 100}" + "%",
+            "NDFilter": mlcm.MLFilterEnum_to_str(nd_enum),
+            "Channel" : channel_name,
+            "AVEGray": average_gray,
+            "ExposureTime": exposure_time,
+            "G/ET": gray_ET,
+            "Luminance": luminance,
+            "K(L)":luminance_k,
+            "Radiance": radiance,
+            "K(R)": radiance_k
+        })
---
+++ ui/mono_calibration_colorcamera_window.py
@@ -0,0 +1,290 @@
+from PyQt5.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QFileDialog,
+    QSizePolicy,
+    QMessageBox,
+    QGroupBox,
+    QGridLayout,
+    QSpacerItem,
+    QRadioButton,
+    QButtonGroup,
+    QCheckBox,
+    QDialog,
+    QComboBox,
+    QFormLayout
+)
+from core.app_config import AppConfig
+from PyQt5.QtCore import pyqtSignal, Qt
+import mlcolorimeter as mlcm
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from openpyxl import Workbook, load_workbook
+from openpyxl.drawing.image import Image
+from scripts.monocalibration_colorcamera import mono_calibration
+from ui.settings import SettingsWindow
+
+class MonoCalibrationColorCameraWindow(QDialog):
+    # path_changed = pyqtSignal(str)
+
+    def __init__(self, path, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("mono calibration Colorcamera")
+        self.setGeometry(200, 200, 800, 500)
+        self.setWindowFlags(Qt.Window | Qt.WindowMaximizeButtonHint | Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
+
+        self.colorimeter = AppConfig.get_colorimeter()
+        self.dialog_title = "选择文件夹"
+        self.default_path = ""
+        self.select_path=path
+        self.file_name = "mono_calibration.xlsx"
+        self.binning_selector=['Logic','Sensor']
+        self.binning_mode=['AVERAGE','SUM']
+        self.pixel_format=['MLMono8','MLMono10','MLMono12','MLMono16','MLRGB24','MLBayer','MLBayerGB8','MLBayerGB12']
+        self._init_ui()
+
+    def _init_ui(self):
+        grid_layout = QGridLayout()
+
+        group_box0=QGroupBox("相机设置")
+        from_layout0=QFormLayout()
+
+        self.label_binn_selector = QLabel(" binning_selector：")
+        self.line_edit_binn_selector = QComboBox()
+        self.line_edit_binn_selector.addItems(self.binning_selector)
+        self.line_edit_binn_selector.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        self.label_binn_mode = QLabel(" binning_mode：")
+        self.line_edit_binn_mode = QComboBox()
+        self.line_edit_binn_mode.addItems(self.binning_mode)
+        self.line_edit_binn_mode.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        horizontal_layout=QHBoxLayout()
+        horizontal_layout.addWidget(self.label_binn_selector)
+        horizontal_layout.addWidget(self.line_edit_binn_selector)
+        horizontal_layout.addWidget(self.label_binn_mode)
+        horizontal_layout.addWidget(self.line_edit_binn_mode)
+        from_layout0.addRow(horizontal_layout)
+
+        self.label_binnlist = QLabel(" binning：")
+        self.line_edit_binnlist = QLineEdit()
+        self.line_edit_binnlist.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.line_edit_binnlist.setText("0")
+        self.line_edit_binnlist.setPlaceholderText("0: 1X1, 1: 2X2, 2: 4X4, 3: 8X8, 4: 16X16")
+        from_layout0.addRow(self.label_binnlist, self.line_edit_binnlist)
+
+        group_box0.setLayout(from_layout0)
+        grid_layout.addWidget(group_box0, 0, 0)
+
+        self.label_aperture = QLabel()
+        self.label_aperture.setText("光阑(mm): 例如：3mm")
+        grid_layout.addWidget(self.label_aperture, 1, 0)
+        self.line_edit_aperture = QLineEdit()
+        self.line_edit_aperture.setText("3mm")
+        self.line_edit_aperture.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_aperture, 2, 0)
+
+        self.label_ndlist = QLabel()
+        self.label_ndlist.setText("nd列表, (4: ND0, 5: ND1, 6: ND2, 7:ND3, 8:ND4), 以空格隔开")
+        grid_layout.addWidget(self.label_ndlist, 3, 0)
+        self.line_edit_ndlist = QLineEdit()
+        self.line_edit_ndlist.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_ndlist, 4, 0)
+
+        self.label_luminance_lum=QLabel()
+        self.label_luminance_lum.setText("Luminance：")
+        grid_layout.addWidget(self.label_luminance_lum, 5, 0)
+        self.line_edit_luminance_lum = QLineEdit()
+        self.line_edit_luminance_lum.setText("1000")
+        self.line_edit_luminance_lum.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)  
+        grid_layout.addWidget(self.line_edit_luminance_lum, 6, 0)
+
+
+        self.label_radiance_lum=QLabel()
+        self.label_radiance_lum.setText("Radiance：")
+        grid_layout.addWidget(self.label_radiance_lum, 7, 0)
+        self.line_edit_radiance_lum = QLineEdit()
+        self.line_edit_radiance_lum.setText("1000")
+        self.line_edit_radiance_lum.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)  
+        grid_layout.addWidget(self.line_edit_radiance_lum, 8, 0)
+
+        self.label_gray_range=QLabel()
+        self.label_gray_range.setText("灰度值(例如: 0.5,0.8)，多个灰度值以空格隔开")
+        grid_layout.addWidget(self.label_gray_range, 9, 0)
+        self.line_edit_gray_range = QLineEdit()
+        self.line_edit_gray_range.setText("0.8")
+        self.line_edit_gray_range.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_gray_range, 10, 0)
+
+        self.label_image_size = QLabel()
+        self.label_image_size.setText("图像中心点坐标x y（打开相机软件查看）：例如像素为13376 9528，则中心点为6688 4764以空格隔开")
+        grid_layout.addWidget(self.label_image_size, 11, 0)
+        self.line_edit_image_size = QLineEdit()
+        self.line_edit_image_size.setText("6688 4764")
+        self.line_edit_image_size.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_image_size, 12, 0)
+
+        self.label_roi_size = QLabel()
+        self.label_roi_size.setText("ROI宽高：例如200 200，以空格隔开")
+        grid_layout.addWidget(self.label_roi_size, 13, 0)
+        self.line_edit_roi_size = QLineEdit()
+        self.line_edit_roi_size.setText("200 200")
+        self.line_edit_roi_size.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_roi_size, 14, 0)
+
+        h_layout = QHBoxLayout()
+        self.cb_R = QRadioButton()
+        self.cb_R.setText("R")
+        self.cb_R.setChecked(False)
+
+        h_layout = QHBoxLayout()
+        self.cb_G = QRadioButton()
+        self.cb_G.setText("G")
+        self.cb_G.setChecked(False)
+
+        h_layout = QHBoxLayout()
+        self.cb_B = QRadioButton()
+        self.cb_B.setText("B")
+        self.cb_B.setChecked(False)
+
+        h_layout = QHBoxLayout()
+        self.cb_W = QRadioButton()
+        self.cb_W.setText("W")
+        self.cb_W.setChecked(True)
+
+        self.rgbw_btngroup = QButtonGroup(self)
+        self.rgbw_btngroup.addButton(self.cb_R, 1)
+        self.rgbw_btngroup.addButton(self.cb_G, 2)
+        self.rgbw_btngroup.addButton(self.cb_B, 3)
+        self.rgbw_btngroup.addButton(self.cb_W, 4)
+
+        h_layout.addWidget(self.cb_R)
+        h_layout.addWidget(self.cb_G)
+        h_layout.addWidget(self.cb_B)
+        h_layout.addWidget(self.cb_W)
+        grid_layout.addLayout(h_layout, 15, 0)
+
+        self.label_path = QLabel()
+        self.label_path.setText("保存路径(excel保存位置):")
+        grid_layout.addWidget(self.label_path, 16, 0)
+
+        self.line_edit_path = QLineEdit()
+        self.line_edit_path.setReadOnly(True)  # 设置为只读
+        self.line_edit_path.setPlaceholderText("未选择文件夹")
+        self.line_edit_path.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.line_edit_path, 17, 0)
+
+        self.btn_browse = QPushButton("浏览...")
+        self.btn_browse.clicked.connect(self._open_folder_dialog)
+        grid_layout.addWidget(self.btn_browse, 17, 1)
+
+        self.label_path=QLabel()
+        self.label_path.setText("配置路径（eye1）")
+        grid_layout.addWidget(self.label_path, 18, 0)
+        self.line_edit_eye1_path=QLineEdit()
+        self.line_edit_eye1_path.setReadOnly(True)
+        self.line_edit_eye1_path.setText(self.select_path)
+        grid_layout.addWidget(self.line_edit_eye1_path,19,0)
+
+
+        self.btn_capture = QPushButton("单色定标")
+        self.btn_capture.clicked.connect(self.start_mono_calibration)
+        grid_layout.addWidget(self.btn_capture, 20, 0)
+
+        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        grid_layout.addItem(spacer)
+
+        self.setLayout(grid_layout)
+
+    def _open_folder_dialog(self):
+        # 打开文件夹选择对话框
+        folder_path = QFileDialog.getExistingDirectory(
+            self,
+            self.dialog_title,
+            self.default_path if self.default_path else "",  # 初始路径
+            options=QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks,
+        )
+
+        if folder_path:
+            self.save_path = folder_path
+            self.line_edit_path.setText(folder_path)
+        else:
+            QMessageBox.critical(self,"MLColorimeter","选择路径错误",QMessageBox.Yes | QMessageBox.No,QMessageBox.Yes)
+
+    def start_mono_calibration(self):
+        try:
+            self.pixel_format=self.get_current_pixel_format()
+            self.binn_selector=self.get_current_binning_selector()
+            self.binn_mode=self.get_current_binning_mode()
+            self.binn=mlcm.Binning(int(self.line_edit_binnlist.text().strip()))
+            # self.path_changed.connect(self.out_path_changed)
+            self.eye1_path=self.line_edit_eye1_path.text()
+            self.luminance=float(self.line_edit_luminance_lum.text())
+            self.aperture = self.line_edit_aperture.text()
+            self.nd_list=self.line_edit_ndlist.text().split()
+            self.light_source=self.rgbw_btngroup.checkedButton().text()
+            self.radiance=float(self.line_edit_radiance_lum.text())
+            self.gray_list=[float(gray) for gray in self.line_edit_gray_range.text().split()]
+            self.image_point=self.line_edit_image_size.text().split()
+            self.roi_size=self.line_edit_roi_size.text().split()
+            self.out_path=self.line_edit_path.text()
+            mono_calibration(
+                colorimeter=self.colorimeter,
+                binn_selector=self.binn_selector,
+                binn_mode=self.binn_mode,
+                binn=self.binn,
+                pixel_format=self.pixel_format,
+                nd_list=self.nd_list,
+                gray_range=self.gray_list,
+                apturate=self.aperture,
+                light_source=self.light_source,
+                luminance=self.luminance,
+                radiance=self.radiance,
+                eye1_path=self.eye1_path,
+                out_path=self.out_path,
+                image_point=self.image_point,
+                roi_size=self.roi_size
+            )
+        except Exception as e:
+            QMessageBox.critical(self,"MLColorimeter","exception" + e, QMessageBox.Yes | QMessageBox.No,QMessageBox.Yes)
+        
+    
+    def get_current_pixel_format(self):
+        # 获取当前选择的项
+        selected_format=self.line_edit_pixel_format.currentText()
+        # 创建字符串到枚举值的映射
+        format_mapping={
+            'MLMono8':mlcm.MLPixelFormat.MLMono8,
+            'MLMono10':mlcm.MLPixelFormat.MLMono10,
+            'MLMono12':mlcm.MLPixelFormat.MLMono12,
+            'MLMono16':mlcm.MLPixelFormat.MLMono16,
+            'MLRGB24':mlcm.MLPixelFormat.MLRGB24,
+            'MLBayer':mlcm.MLPixelFormat.MLBayer,
+            'MLBayerGB8':mlcm.MLPixelFormat.MLBayerGB8,
+            'MLBayerGB12':mlcm.MLPixelFormat.MLBayerGB12,
+        }
+        # 获取对应的枚举值
+        pixel_format_enum=format_mapping.get(selected_format)
+        return pixel_format_enum
+    
+    def get_current_binning_selector(self):
+        # 获取当前选择的项
+        selected_selector=self.line_edit_binn_selector.currentText()
+        if selected_selector=='Logic':
+            return mlcm.BinningSelector.Logic
+        else:
+            return mlcm.BinningSelector.Sensor
+        
+    def get_current_binning_mode(self):
+        # 获取当前选择的项
+        selected_mode=self.line_edit_binn_mode.currentText()
+        if selected_mode=='AVERAGE':
+            return mlcm.BinningMode.AVERAGE
+        else:
+            return mlcm.BinningMode.SUM
