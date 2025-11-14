992a7086b2466bb6131270369455397fed635313
su-shifang <shifang.su@mloptic.co>
2025-11-14 10:37:15 +0800

    bug:repeat select EYE1 show "ml_add_module error" solve:remove module before add module

--- ui/settings_window.py
+++ ui/settings_window.py
@@ -24,7 +24,8 @@ class SettingsWindow(QDialog):
         self.setGeometry(300, 300, 500, 200)
         self.setWindowFlags(Qt.Window | Qt.WindowMaximizeButtonHint | Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
         self.dialog_title = dialog_title
-        self.default_path = default_path
+        self.default_path = default_path    
+        self.previous_path=""
         self.colorimeter = AppConfig.get_colorimeter()
         self._init_ui()
 
@@ -62,9 +63,11 @@ class SettingsWindow(QDialog):
 
         if folder_path:
             self.set_path(folder_path)
+            ret = self.colorimeter.ml_remove_module([1])
             ret = self.colorimeter.ml_add_module(path_list=[folder_path])
             if not ret.success:
                 QMessageBox.critical(self,"MLColorimeter","ml_add_module error",QMessageBox.Yes | QMessageBox.No,QMessageBox.Yes)
+                
             else:
                 # 发射信号，启用连接按钮
                 self.enables_connect_button.emit()
